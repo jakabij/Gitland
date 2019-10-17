3dd5859be4ea869cfa25a6baeef09aa99fa6f3d0
Erik Aron Szabo <erik.aron.szabo@gmail.com>
2019-10-17 09:27:01 +0200

    Joke added

--- gittie.py
+++ gittie.py
@@ -1,2 +1,24 @@
+import time
 def introduce():
     print("Hello, I'm Gittie!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def joke():
+    print("What day eggs hate the most?")
+    time.sleep(5.3)
+    print("Fry-day.")
